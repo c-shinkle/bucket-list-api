4b94101cc662da0cba72e4b3bd930c8bf7958443
Christian Shinkle <cshinkle2018@gmail.com>
2021-07-25 22:47:56 -0500

    added mock database for ui testing

--- src/api/server.ts
+++ src/api/server.ts
@@ -1,8 +1,21 @@
-import express from "express";
+import express, {Request, Response} from "express";
+import { Dictionary } from "lodash";
+
 const server = express();
 
-server.get('/', (_, res: express.Response): void => {
-    res.send('Hello, World!');
+const database: Dictionary<string[]> = {
+    '1': ['Visit Paris', 'Taste French Wine', 'Visit German Castles', 'Remember those who have fallen at Normandy'],
+    '2': ['Climb Mount Everest', 'Scuba Dive in the Great Barrier Reef']
+}
+
+server.get('/bucket-list/:id', (req: Request, res: Response): void => {
+    const maybeBucketList = database[req.params.id];
+    if (maybeBucketList) {
+        res.header("Access-Control-Allow-Origin", "*");
+        res.send(maybeBucketList);
+    } else {
+        res.status(404).send("Not Found");
+    }
 });
 
 export default server;
